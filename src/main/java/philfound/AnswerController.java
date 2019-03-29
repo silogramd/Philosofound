541d8b86f972a3d9b72df5d18d9a5b4c4c71dfa8
silogramd <silogramd@users.noreply.github.com>
2019-03-29 17:42:56 -0400

    Update AnswerController.java

--- src/main/java/philfound/AnswerController.java
+++ src/main/java/philfound/AnswerController.java
@@ -1,9 +1,9 @@
-package com.example.jpa.controller;
+package philfound.jpa.controller;
 
-import com.example.jpa.exception.ResourceNotFoundException;
-import com.example.jpa.model.Comment;
-import com.example.jpa.repository.CommentRepository;
-import com.example.jpa.repository.PostRepository;
+import philfound.jpa.exception.ResourceNotFoundException;
+import philfound.jpa.model.Answer;
+import philfound.jpa.repository.AnswerRepository;
+import philfound.jpa.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,49 +13,49 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-public class CommentController {
+public class AnswerController {
 
     @Autowired
-    private CommentRepository commentRepository;
+    private AnswerRepository answerRepository;
 
     @Autowired
-    private PostRepository postRepository;
+    private QuestionRepository questionRepository;
 
-    @GetMapping("/posts/{postId}/comments")
-    public Page<Comment> getAllCommentsByPostId(@PathVariable (value = "postId") Long postId,
+    @GetMapping("/questions/{questionId}/answers")
+    public Page<Comment> getAllAnswersByQuestionID(@PathVariable (value = "questionID") Long questionID,
                                                 Pageable pageable) {
-        return commentRepository.findByPostId(postId, pageable);
+        return questionRepository.findByQuestionID(questionID, pageable);
     }
 
-    @PostMapping("/posts/{postId}/comments")
-    public Comment createComment(@PathVariable (value = "postId") Long postId,
-                                 @Valid @RequestBody Comment comment) {
-        return postRepository.findById(postId).map(post -> {
-            comment.setPost(post);
-            return commentRepository.save(comment);
-        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    @PostMapping("/questions/{questionId}/answers")
+    public Answer createAnswers(@PathVariable (value = "quesionId") Long questionId,
+                                 @Valid @RequestBody Answer answer) {
+        return questionRepository.findById(questionID).map(question -> {
+            answer.setQuestion(question);
+            return answerRepository.save(answer);
+        }).orElseThrow(() -> new ResourceNotFoundException("QuestionID " + questionId + " not found"));
     }
 
-    @PutMapping("/posts/{postId}/comments/{commentId}")
-    public Comment updateComment(@PathVariable (value = "postId") Long postId,
-                                 @PathVariable (value = "commentId") Long commentId,
-                                 @Valid @RequestBody Comment commentRequest) {
-        if(!postRepository.existsById(postId)) {
-            throw new ResourceNotFoundException("PostId " + postId + " not found");
+    @PutMapping("/questions/{questionId}/answers/{answerId}")
+    public Answer updateAnswer(@PathVariable (value = "questionId") Long question,
+                                 @PathVariable (value = "answerID") Long answerId,
+                                 @Valid @RequestBody Answer answerRequest) {
+        if(!questionRepository.existsById(questionID)) {
+            throw new ResourceNotFoundException("QuestionID " + questionID + " not found");
         }
 
-        return commentRepository.findById(commentId).map(comment -> {
-            comment.setText(commentRequest.getText());
-            return commentRepository.save(comment);
-        }).orElseThrow(() -> new ResourceNotFoundException("CommentId " + commentId + "not found"));
+        return answerRepository.findById(answerID).map(answer -> {
+            answer.setText(answerRequest.getText());
+            return answerRepository.save(answer);
+        }).orElseThrow(() -> new ResourceNotFoundException("AnswerID " + answerId + "not found"));
     }
 
-    @DeleteMapping("/posts/{postId}/comments/{commentId}")
-    public ResponseEntity<?> deleteComment(@PathVariable (value = "postId") Long postId,
-                              @PathVariable (value = "commentId") Long commentId) {
-        return commentRepository.findByIdAndPostId(commentId, postId).map(comment -> {
-            commentRepository.delete(comment);
+    @DeleteMapping("/questions/{questionId}/answers/{answerId}")
+    public ResponseEntity<?> deleteAnswer(@PathVariable (value = "answerID") Long answerID,
+                              @PathVariable (value = "answerID") Long answerID) {
+        return answerRepository.findByIdAndQuestionId(answerId, questionId).map(comment -> {
+            questionRepository.delete(answer);
             return ResponseEntity.ok().build();
-        }).orElseThrow(() -> new ResourceNotFoundException("Comment not found with id " + commentId + " and postId " + postId));
+        }).orElseThrow(() -> new ResourceNotFoundException("Answer not found with id " + answerID + " and questionID " + setQuestionId));
     }
 }
