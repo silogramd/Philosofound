56a74824798b418e0af350824ef9db636d0679db
jakechvatal <jakechvatal@gmail.com>
2019-03-30 18:23:42 -0500

    removed img reference

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import QuestionMenu from './questionMenu.js'
 import AppBar from './AppBar.js'
