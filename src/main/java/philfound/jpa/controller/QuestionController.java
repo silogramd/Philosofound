e63322ab61e726f855d90ccf1b93870a36c0912e
silogramd <silogramd@users.noreply.github.com>
2019-04-03 15:04:16 -0400

    Update QuestionController.java

--- src/main/java/philfound/jpa/controller/QuestionController.java
+++ src/main/java/philfound/jpa/controller/QuestionController.java
@@ -32,7 +32,7 @@ public class QuestionController {
     @GetMapping("/users/{userId}/questions")
     public Page<Question> getAllQuestionsByUserId(@PathVariable(value = "userId") Long userId,
                                                 Pageable pageable) {
-        return questionRepository.findByUser(userRepository.findById(userId), pageable);
+        return questionRepository.findByUser(userRepository.findById(userId).orElseThrow(new IllegalArgumentException("No user found")), pageable);
     }
 
     @PostMapping("/questions/{userId}")
